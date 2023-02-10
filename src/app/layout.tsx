0a89282850b77a4668fa6ce783d05d63ce0e7819
Brett Mandler <brettmandler@gmail.com>
2023-02-10 17:37:37 -0500

    Added plausable analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,13 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-        <head />
+        <head>
+          <script
+            defer
+            data-domain="livewellwithmandy.com"
+            src="https://plausible.io/js/script.js"
+          ></script>
+        </head>
 
         <body className={inter.className}>
           <div className="w-full bg-gray-50 text-gray-700 px-3 md:px-0 dark:bg-gray-900 dark:text-gray-50 transition-colors ease-linear min-h-screen">
