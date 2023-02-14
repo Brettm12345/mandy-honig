ae90bdb3a4bcde83cf0ea9b1f55ef3aa6cc39c75
Brett Mandler <brettmandler@gmail.com>
2023-02-14 14:06:39 -0500

    Added space between blog posts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,30 @@
-import {Calendar} from 'lucide-react'
+import { Calendar } from "lucide-react";
 
-import {allBlogs} from 'contentlayer/generated'
-import Image from 'next/image'
-import Link from 'next/link'
-import dayjs from 'dayjs'
-import {Post} from '@/components/Post'
+import { allBlogs } from "contentlayer/generated";
+import Image from "next/image";
+import Link from "next/link";
+import dayjs from "dayjs";
+import { Post } from "@/components/Post";
 
 export const metadata = {
-  description: 'A blog for health, fitness and sobriety advice.',
+  description: "A blog for health, fitness and sobriety advice.",
   openGraph: {
-    title: 'Mandy Honig',
-    description: 'A blog for health, fitness and sobriety advice.',
-    url: 'https://mandy.vercel.app',
-    siteName: 'Mandy Honig',
-    locale: 'en-US',
-    type: 'website',
+    title: "Mandy Honig",
+    description: "A blog for health, fitness and sobriety advice.",
+    url: "https://mandy.vercel.app",
+    siteName: "Mandy Honig",
+    locale: "en-US",
+    type: "website",
   },
-}
+};
 export default function Home() {
   return (
     <div>
-      <ul>
-        {allBlogs.map(blog => (
+      <ul className="space-x-6">
+        {allBlogs.map((blog) => (
           <Post key={blog.slug} blog={blog} />
         ))}
       </ul>
     </div>
-  )
+  );
 }
