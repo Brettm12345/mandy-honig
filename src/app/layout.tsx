e990b4764cc791b2e1c135fc148e33e8d3412575
Brett Mandler <brettmandler@gmail.com>
2023-02-17 11:19:05 -0500

    Fix dark mode flash

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import {ServerThemeProvider} from '@wits/next-themes'
 import {Inter} from '@next/font/google'
 import './globals.css'
 import {ThemeToggle} from '@/components/ThemeToggle'
+import Providers from '@/components/Providers'
 
 const inter = Inter({subsets: ['latin']})
 export default function RootLayout({children}: {children: React.ReactNode}) {
@@ -18,42 +19,45 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
         </head>
 
         <body className={inter.className}>
-          <div className="w-full bg-gray-50 text-gray-700 px-3 md:px-0 dark:bg-gray-900 dark:text-gray-50 transition-colors ease-linear min-h-screen">
-            <div className="max-w-screen-md mx-auto">
-              <div className="border-b border-gray-300 dark:border-gray-800 pb-4">
-                <div className="flex flex-row justify-between items-center">
-                  <div className="pt-16 pb-8">
-                    <div className="font-semibold text-3xl text-gray-900 dark:text-gray-50">
-                      Mandy Honig
+          <Providers>
+            <div className="w-full bg-gray-50 text-gray-700 px-3 md:px-0 dark:bg-gray-900 dark:text-gray-50 transition-colors ease-linear min-h-screen">
+              <div className="max-w-screen-md mx-auto">
+                <div className="border-b border-gray-300 dark:border-gray-800 pb-4">
+                  <div className="flex flex-row justify-between items-center">
+                    <div className="pt-16 pb-8">
+                      <div className="font-semibold text-3xl text-gray-900 dark:text-gray-50">
+                        Mandy Honig
+                      </div>
+                      <div className="text-xl">
+                        A blog for health, fitness and sobriety advice.
+                      </div>
                     </div>
-                    <div className="text-xl">
-                      A blog for health, fitness and sobriety advice.
-                    </div>
+                    <ThemeToggle />
                   </div>
-                  <ThemeToggle />
+                  <div>
+                    <ul className="flex text-xl space-x-6">
+                      <li>
+                        <Link href="/">Home</Link>
+                      </li>
+                      <li>
+                        <Link href="/about/">About</Link>
+                      </li>
+                    </ul>
+                  </div>
                 </div>
-                <div>
-                  <ul className="flex text-xl space-x-6">
-                    <li>
-                      <Link href="/">Home</Link>
-                    </li>
-                    <li>
-                      <Link href="/about/">About</Link>
-                    </li>
-                  </ul>
+
+                <div className="text-xl py-5">{children}</div>
+
+                <div className="border-t border-gray-300 dark:border-gray-800 text-center py-8 text-sm">
+                  © Copyright {new Date().getFullYear()} Mandy Honig. Powered
+                  with{' '}
+                  <span role="img" aria-label="Love">
+                    ♥
+                  </span>
                 </div>
-              </div>
-
-              <div className="text-xl py-5">{children}</div>
-
-              <div className="border-t border-gray-300 dark:border-gray-800 text-center py-8 text-sm">
-                © Copyright {new Date().getFullYear()} Mandy Honig. Powered with{' '}
-                <span role="img" aria-label="Love">
-                  ♥
-                </span>
               </div>
             </div>
-          </div>
+          </Providers>
         </body>
       </html>
     </ServerThemeProvider>
